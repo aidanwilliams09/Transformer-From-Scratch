675c1da0eb6bd27ed327ac82e48e847238df95ae
Aidan Williams <aidan19@me.com>
2022-04-13 16:04:19 -0400

    Update setup

--- setup.py
+++ setup.py
@@ -5,19 +5,16 @@ with open("README.md", "r", encoding="utf-8") as fh:
     
 setuptools.setup(
     name='transformer',
-    version='0.0.1',
+    version='0.0.2',
     author='552 Group 2',
     author_email='aidan.williams@mail.mcgill.ca',
     description='Transformer model for weather prediction',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='',
+    url='https://github.com/aidanwilliams09/Transformer-From-Scratch',
     license='MIT',
     packages=['transformer_552'],
     install_requires=[
-        'numpy',
-        'scikit-learn',
         'torch',
-        'torchvision'
     ],
 )
